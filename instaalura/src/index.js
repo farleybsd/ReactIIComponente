17cb73a1a639e775cefa7f9c9f8cd49c96bbde88
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-12-25 17:21:48 -0300

    Criação da Tela de login

--- instaalura/src/index.js
+++ instaalura/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/reset.css'
 import './CSS/timeline.css'
+import './CSS/login.css'
 import App from './App';
+import Login from './componentes/Login'
+import {Router,Route,browserHistory} from 'react-router';
 
-
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( 
+    (
+    <Router history ={browserHistory}>
+        <Route path="/" component={Login} />
+        <Route path="/timeline" component={App}/>
+    </Router>
+    
+    ) , document.getElementById('root'));
