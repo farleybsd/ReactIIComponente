7f65e50208654cb4a64143d51b306e5c032625e2
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-12-27 12:13:08 -0300

    Navegação programática e JWT

--- instaalura/src/componentes/Login.js
+++ instaalura/src/componentes/Login.js
@@ -1,53 +1,52 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react';
+import {browserHistory} from  'react-router';
 
-export default  class Login extends Component{
-    
+export default class Login extends Component {
 
-    constructor(){
-        super()
-        this.state = {msg:""}
+    constructor(props){
+        super(props);        
+        this.state = {msg:this.props.location.query.msg};
     }
 
     envia(event){
-
         event.preventDefault();
 
         const requestInfo = {
-            method: 'POST',
-            body: JSON.stringify({login:this.Login.value,senha:this.Senha.value}),
-            headers: new Headers({
-
-                'Content-type': 'application/json'
-
+            method:'POST',
+            body:JSON.stringify({login:this.login.value,senha:this.senha.value}),
+            headers:new Headers({
+                'Content-type' : 'application/json' 
             })
-        }
+        };
 
         fetch('http://localhost:8080/api/public/login',requestInfo)
-          .then(response => {
-              if(response.ok){
-                  return response.text();
-
-              } else{
-                this.setState({msg:'Não foi possivél Realizar o Login!'})
-              }
-          })
-            .then(token =>{
-             console.log(token)   
+            .then(response => {
+                if(response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error('não foi possível fazer o login');
+                }
             })
+            .then(token => {
+                localStorage.setItem('auth-token',token);
+                browserHistory.push('/timeline');
+            })
+            .catch(error => {
+                this.setState({msg:error.message});
+            });
     }
 
     render(){
-        return(
-        <div className="login-box">
-            <h1 className="header-logo">Instalura</h1>
-        <span>{this.state.msg}</span>
-            <form onSubmit= {this.envia.bind(this)}> 
-             <input type="text" ref={(input) => this.Login = input} />
-             <input type="password" ref={(input) => this.Senha = input}/>
-             <input type="submit" value="login"/>
-          </form>
-        </div>
-
-        )
+        return (
+            <div className="login-box">
+                <h1 className="header-logo">Instalura</h1>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input}/>
+                    <input type="password" ref={(input) => this.senha = input}/>
+                    <input type="submit" value="login"/>
+                </form>
+            </div>
+        );
     }
 }
--- instaalura/src/componentes/TimeLine.js
+++ instaalura/src/componentes/TimeLine.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
-import FotoItem from './Foto'
-export default class TimeLine extends Component {
+import FotoItem from './Foto';
+
+export default class Timeline extends Component {
 
     constructor(){
-        super()
-        this.state = {fotos:[]}
+      super();
+      this.state = {fotos:[]};
     }
 
     componentDidMount(){
-        fetch('http://localhost:8080/api/public/fotos/rafael')
-         .then(response => response.json())
-         .then(fotos => {
-             this.setState({fotos:fotos})
-         })
+      // fetch('http://localhost:8080/api/public/fotos/rafael')  
+      fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+       .then(response => response.json())
+       .then(fotos => {
+         this.setState({fotos:fotos});
+       });
     }
 
-    render() {
-
+    render(){
         return (
-            <div className="fotos container">
-               
-                {
-                    this.state.fotos.map(foto => <FotoItem foto ={foto}/>)
-                }
-            </div>
-        )
-
+        <div className="fotos container">
+          {
+            this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto}/>)
+          }                
+        </div>            
+        );
     }
 }
