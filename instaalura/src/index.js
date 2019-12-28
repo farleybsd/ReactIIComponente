dafda8ab96ceb1963bb5c02c043940e1aaf24b2b
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-12-28 11:53:27 -0300

    Protegendo urls e possibilitando o logout PRÓXIMA ATIVIDADE

--- instaalura/src/index.js
+++ instaalura/src/index.js
@@ -5,13 +5,23 @@ import './CSS/timeline.css'
 import './CSS/login.css'
 import App from './App';
 import Login from './componentes/Login'
+import Logout from './componentes/Logout'
 import {Router,Route,browserHistory} from 'react-router';
+
+
+function verificaAutenticacao(nexState,replace){
+
+    if(localStorage.getItem('auth-token') === null){
+        replace('/?msg=Você precisa estar logado para acessar o endereco')
+    }
+}
 
 ReactDOM.render( 
     (
     <Router history ={browserHistory}>
         <Route path="/" component={Login} />
-        <Route path="/timeline" component={App}/>
+        <Route path="/timeline" component={App} onEnter ={ verificaAutenticacao}/>
+        <Route path="/logout" component={Logout} />
     </Router>
     
     ) , document.getElementById('root'));
