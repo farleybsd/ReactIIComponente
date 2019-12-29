16b0ef1a9c070f3e5f259d948b759acd2d3d05af
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-12-28 22:01:06 -0300

    Implementaçao do like - Parte 2

--- instaalura/src/componentes/Foto.js
+++ instaalura/src/componentes/Foto.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router'
+import Pubsub from 'pubsub-js';
 class FotoAtualizacoes extends Component {
 
 constructor(props){
@@ -19,12 +20,15 @@ constructor(props){
           throw new Error("Não Foi Possivel Realizar o Like da Foto!")
         }
       })
-      .then(like =>{
+      .then(liker =>{
         //console.log(like)
         this.setState({
           likeada : !this.state.likeada
 
         })
+
+        Pubsub.publish('atualiza-liker',{fotoId:this.props.foto.id,liker})
+
       })
 
   }
@@ -45,13 +49,39 @@ constructor(props){
 }
 
 class FotoInfo extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { likers: this.props.foto.likers }
+  }
+
+  componentWillMount() {
+    Pubsub.subscribe('atualiza-liker', (topico, infoLiker) => {
+
+      //console.log(infoLiker)
+      if (this.props.foto.id === infoLiker.fotoId) {
+        const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login);
+        //console.log(possivelLiker)
+        if (possivelLiker === undefined) {
+          const novosLikers = this.state.likers.concat(infoLiker.liker)
+         this.setState({likers : novosLikers})
+
+        } 
+      }else {
+        const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
+        this.setState({likers:novosLikers});
+    }  
+    })
+
+  }
+
     render(){
         return (
-            <div className="foto-in fo">
+            <div className="foto-info">
               <div className="foto-info-likes">
                 {
-                  this.props.foto.likers.map(liker => {
-                    return (<Link key={liker.login} href={`/timeline/${liker.login}`}>{liker.login}</Link>)
+                  this.state.likers.map(liker => {
+                    return (<a key={liker.login} href="#">{liker.login},</a> )
                   })
                 }
                  
